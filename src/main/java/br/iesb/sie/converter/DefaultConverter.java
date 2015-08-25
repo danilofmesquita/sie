31494d6e0c2901837a9c24b4c9224b85370c4938
d4nilol <danilofmesquit@gmail.com>
2015-08-24 21:53:09 -0300

    correções

--- src/main/java/br/iesb/sie/converter/DefaultConverter.java
+++ src/main/java/br/iesb/sie/converter/DefaultConverter.java
@@ -11,11 +11,16 @@ public class DefaultConverter implements Converter {
 
     @Override
     public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, String value) {
-        return value;
+        return uiComponent.getAttributes().get(value);
     }
 
     @Override
     public String getAsString(FacesContext facesContext, UIComponent uiComponent, Object value) {
-        return String.valueOf(value);
+        if (value == null) {
+            return null;
+        }
+        String hash = String.valueOf(value.hashCode());
+        uiComponent.getAttributes().put(hash, value);
+        return hash;
     }
 }
