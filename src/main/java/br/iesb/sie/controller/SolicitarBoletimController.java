db48a61e9eb90c2471db492ca95f1cb3dfd8e977
d4nilol <danilofmesquita@gmail.com>
2015-10-09 22:56:34 -0300

    Implementando Bolebim

--- src/main/java/br/iesb/sie/controller/SolicitarBoletimController.java
+++ src/main/java/br/iesb/sie/controller/SolicitarBoletimController.java
@@ -1,10 +1,78 @@
 package br.iesb.sie.controller;
 
+import br.iesb.sie.bean.UsuarioLogado;
+import br.iesb.sie.dto.BoletimDTO;
+import br.iesb.sie.dto.PasseDTO;
+import br.iesb.sie.entity.Entidade;
+import br.iesb.sie.entity.Turma;
+import br.iesb.sie.service.EntidadeService;
+import br.iesb.sie.service.JasperReportsService;
+import br.iesb.sie.service.TurmaService;
+import br.iesb.sie.util.DownloadUtil;
+import net.sf.jasperreports.engine.JRException;
+
+import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Named
 @ViewScoped
 public class SolicitarBoletimController extends BaseController {
+
+    @Inject
+    private EntidadeService entidadeService;
+
+    @Inject
+    private TurmaService turmaService;
+
+    @Inject
+    private UsuarioLogado usuarioLogado;
+
+    @Inject
+    private JasperReportsService jasperReportsService;
+
+    private BoletimDTO boletimDTO;
+
+    @PostConstruct
+    public void init() {
+        boletimDTO = new BoletimDTO();
+    }
+
+    public List<Entidade> getEscolasVinculadas() {
+        return entidadeService.buscarEscolasVinculadasAoAluno(usuarioLogado.getEntidade());
+    }
+
+    public List<Turma> getTurmasVinculadas() {
+        if (boletimDTO.getEscola() != null) {
+            return turmaService.buscarTurmasVinculadasAEscolaEAluno(boletimDTO.getEscola(), usuarioLogado.getEntidade());
+        } else {
+            return Collections.emptyList();
+        }
+    }
+
+    public void imprimir() throws JRException {
+        Map<String, Object> params = new HashMap<>();
+
+        params.put("ID_ALUNO", usuarioLogado.getEntidade().getId());
+        params.put("ID_ESCOLA", boletimDTO.getEscola().getId());
+        params.put("ID_TURMA", boletimDTO.getTurma().getId());
+
+        byte[] bytes = jasperReportsService.gerarRelatorio(JasperReportsService.BOLETIM, params);
+
+        new DownloadUtil().download(getFacesContext(), DownloadUtil.PDF_TYPE, bytes, "boletim.pdf");
+    }
+
+    public BoletimDTO getBoletimDTO() {
+        return boletimDTO;
+    }
+
+    public void setBoletimDTO(BoletimDTO boletimDTO) {
+        this.boletimDTO = boletimDTO;
+    }
 
 }
